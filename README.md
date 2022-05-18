cd44110ee83ac46a4cccd5a7e9d82f990dbe54bc
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-05-18 17:37:14 +0530

    express: route params

---
+++ README.md
@@ -0,0 +1,1 @@
+## Express Reference 📗
--- server.js
+++ server.js
@@ -7,9 +7,34 @@ const app = express();
 // Initializing PORT
 const PORT = 3000;
 
-///////////////////// ROUTES
-app.get("/", (req, res) => {
-  res.send("Hello");
+// Array Friends //////////////////
+const friends = [
+  {
+    id: 0,
+    name: "Sir Albert Einstein",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+];
+
+// ROUTING ///////////////////
+app.get("/friends", (req, res) => {
+  res.status(200).json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist",
+    });
+  }
 });
 
 app.get("/messages", (req, res) => {
